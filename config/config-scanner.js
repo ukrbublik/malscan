274e70ca870ae1065e27b6c977475f8c96be9732
Denis Oblogin <ukrbublik@gmail.com>
2017-01-15 14:57:53 +0200

    hide my config

--- config/config-scanner.js
+++ config/config-scanner.js
@@ -2,50 +2,6 @@ var isTest = (process.env.isTest == '1');
 
 //----------------------------------------
 
-var webProxies = {
-  Glype: {
-    //'http://www.secretproxy.org': {speed: 5, stable: 0}, //not stable
-    /* bad:
-    'http://www.spysurfing.com': {speed: 5, stable: 1},  
-    'https://proxyweb.me': {speed: 4, stable: 1}, 
-    'https://web-proxy.ro': {speed: 4, stable: 1}, 
-    'http://proxprox.com': {speed: 4, stable: 1}, 
-    'https://zproxy.de': {speed: 1, stable: 1}, 
-    'https://www.magiccloak.net': {speed: 4},
-    'https://0xproxy.com': {speed: 5}, //xml err
-    'http://www.unblock-pakistan.com': {speed: 4},
-    'https://www.proxy62.com': {speed: 4},
-    'http://unblockwebsites.us': {speed: 5},
-    'http://proxyguru.info': {speed: 4},
-    'http://xtcsoul.net': {speed: 5}, 
-    'https://www.proxytime.net': {speed: 3},
-    'http://7proxysites.com': {speed: 3},
-    'https://www.unblocks.net': {speed: 2},
-    'https://www.youcanhide.net': {speed: 2},
-    'http://proxyzan.info': {speed: 1},
-    'https://www.hidemenow.net': {speed: 2},
-    'https://www.kproxy.asia': {speed: 1},
-    'http://www.proxygogo.info': {speed: 1},
-    */
-  },
-  PHProxy: {
-    'http://www.proproxy.me': {speed: 5},
-    'http://ncprox.com': {speed: 3, stable: 1},
-    'http://hidefromyou.com': {speed: 5},
-    /* bad:
-    'http://list-proxy.com': {speed: 5},
-    'http://skinftw.com': {speed: 5},
-    */
-  }
-};
-
-//----------------------------------------
-
-var proxies = {
-};
-
-//----------------------------------------
-
 var config = {
   isTest: isTest,
 	db: {
@@ -76,15 +32,16 @@ var config = {
     },
     /*
     //examples:
-    loc_api_cli: {
+    api_cli_1: {
       type: "apiClient",
-      addr: "http://localhost:8800",
+      addr: "http://1.1.1.1:8800",
     },
-    some_web_proxy: {
+    web_proxy_1: {
       type: "webProxy",
-      webProxyType: "Glype",
+      webProxyType: "Glype", //Glype, PHProxy
+      addr: 'http://www.secretproxy.org',
     },
-    some_proxy: {
+    proxy_1: {
       type: "proxy",
       addr: "1.1.1.1:3128",
     },
@@ -113,7 +70,6 @@ for (let taskName of quickTasks) {
   config.tasks[taskName] = {
     queueSizeConcurrent: 60,
     parserQueueSizeConcurrent: 40,
-    //maxHerokuInstances: 3, // 60 (max speed for heroku) / 20 (speed per 1 inst)
   };
 }
 
@@ -122,65 +78,10 @@ for (let taskName of slowTasks1) {
   config.tasks[taskName] = {
     queueSizeConcurrent: 20,
     parserQueueSizeConcurrent: 3,
-    //maxHerokuInstances: 50, // 60 / 1.58
   };
 }
 
 //----------------------------------------
-
-if (!isTest && 1) {
-  for (let i = 101 ; i <= 120 ; i++) {
-    let k = 'hk'+i;
-    config.providers[k] = {
-      type: "apiClient",
-      serverType: "heroku",
-      addr: "http://mal-api-server-" + i + ".herokuapp.com",
-    };
-  }
-}
-
-var queueSizesForProxySpeeds = {
-  5: [4, 10],
-  4: [4, 10],
-  3: [3, 10],
-  2: [2, 10],
-  1: [2, 10],
-};
-
-if (!isTest && 1) {
-  for (let webProxyType in webProxies) {
-    for (let addr in webProxies[webProxyType]) {
-      let opts = webProxies[webProxyType][addr];
-      if (opts.speed == 0)
-        continue;
-      let k = /^(https?:\/\/)?([\d\w\.-]+)/.exec(addr)[2].replace(/[.-]/g, '_');
-      let conf = {
-        type: "webProxy",
-        webProxyType: webProxyType,
-        addr: addr.replace(/\/$/, ""),
-      };
-      [conf.queueSizeConcurrent, conf.parserQueueSizeConcurrent] = 
-        queueSizesForProxySpeeds[opts.speed];
-      config.providers[k] = conf;
-    }
-  }
-}
-
-if (!isTest && 1) {
-  for (let addr in proxies) {
-    let opts = proxies[addr];
-    if (opts.speed == 0)
-      continue;
-    let k = /^(https?:\/\/)?([\d\w\.-]+)/.exec(addr)[2].replace(/[.\-\:]/g, '_');
-    let conf = {
-      type: "proxy",
-      addr: addr,
-    };
-    [conf.queueSizeConcurrent, conf.parserQueueSizeConcurrent] = 
-      queueSizesForProxySpeeds[opts.speed];
-    config.providers[k] = conf;
-  }
-}
 
 if (isTest) {
   for (let i = 0 ; i <= 10 ; i++) {
--- index.js
+++ index.js
@@ -13,50 +13,62 @@ const http = require('http');
 const app = express();
 const server = http.Server(app);
 const nodeCleanup = require('node-cleanup');
+const args = process.argv.slice(2);
 
-const config = require('./config/config-scanner');
+let configPrefix = process.env.configPrefix ? process.env.configPrefix : 'scanner';
+var config = require('./config/config-'+configPrefix);
 
-var tq = new MalTaskQueue();
-tq.init(config).then(() => {
-  tq.runTaskQueueLoop();
+let taskType = args[0] ? args[0] : 'start';
 
-  //todo - add tasks to queue by timer or manually from redis
-  //tq.addTasksToQueue(MalScanner.grabNewsTasksKeys);
-  //tq.addTasksToQueue(MalScanner.grabUpdatesTasksKeys);
-
-  
-/*
-  //to test proxy
-  for (let id in tq.allScanners) {
-    let sc = tq.allScanners[id];
-    if (sc.provider.options.type == "webProxy") {
-      sc.provider.loadRss("https://myanimelist.net/rss.php?type=rw&u=SesshouNoKon")
-      .then((res) => {
-        console.log(sc.id, 'rss ok');
-      }).catch((err) => {
-        console.log(sc.id, 'rss err', err);
-      });
-      sc.provider.loadXml("https://myanimelist.net/malappinfo.php?u=DreASU&status=all&type=anime")
-      .then((res) => {
-        console.log(sc.id, 'xml ok');
-      }).catch((err) => {
-        console.log(sc.id, 'xml err', err);
-      });
-      sc.provider.loadHtml("https://myanimelist.net/anime/128/some_title")
-      .then(($) => {
-        if ($('a[href*="/anime/genre/1/Action"]').length > 0)
-          console.log(sc.id, 'html ok');
-        else
-          console.log(sc.id, 'html bad $');
-      }).catch((err) => {
-        console.log(sc.id, 'html err', err);
-      });
+if(taskType == 'help') {
+  show_cmd_help();
+} else {
+  var tq = new MalTaskQueue();
+  tq.init(config).then(() => {
+    if (taskType == 'test') {
+      //test providers
+      let typesToTest = ["webProxy", "proxy", "apiClient", "parser"];
+      for (let id in tq.allScanners) {
+        let sc = tq.allScanners[id];
+        if (typesToTest.indexOf(sc.provider.options.type) != -1) {
+          sc.provider.loadRss("https://myanimelist.net/rss.php?type=rw&u=SesshouNoKon")
+          .then((res) => {
+            console.log(sc.id, 'rss ok');
+          }).catch((err) => {
+            console.log(sc.id, 'rss err', err);
+          });
+          sc.provider.loadXml("https://myanimelist.net/malappinfo.php?u=DreASU&status=all&type=anime")
+          .then((res) => {
+            console.log(sc.id, 'xml ok');
+          }).catch((err) => {
+            console.log(sc.id, 'xml err', err);
+          });
+          sc.provider.loadHtml("https://myanimelist.net/anime/128/some_title")
+          .then(($) => {
+            if ($('a[href*="/anime/genre/1/Action"]').length > 0)
+              console.log(sc.id, 'html ok');
+            else
+              console.log(sc.id, 'html bad $');
+          }).catch((err) => {
+            console.log(sc.id, 'html err', err);
+          });
+        }
+      }
+    } else {
+      //run queue loop
+      tq.runTaskQueueLoop();
     }
-  }
-*/
+  });
+}
 
-});
-
+function show_cmd_help() {
+  console.log("Usage: configPrefix=.. " 
+    + "node index.js start|test|help");
+  console.log("configPrefix: scanner (default)");
+  console.log("start: run queue loop");
+  console.log("test: test providers");
+  process.exit(0);
+}
 
 nodeCleanup(() => {
 });
